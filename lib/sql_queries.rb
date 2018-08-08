9281bb403304e7585951bf1cee91e3452b2a9bf9
Brandon Green <brandon@brandonleeproductions.com>
2018-08-08 23:57:19 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -10,7 +10,7 @@ def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_
 "SELECT title, amount
 FROM projects
 INNER JOIN pledges
-JOIN projects ON projects.id = pledges.project_id
+ON projects.id = pledges.project_id
 ORDER BY projects.title, pledges.amount;"
 end
 
